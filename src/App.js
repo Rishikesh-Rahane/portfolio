63ac010a5fc0eb55cf0395accd62bdf144b585cb
Rishikesh-Rahane <rahanerishikesh63@gmail.com>
2024-06-07 10:55:40 +0530

    beta1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="overlay">
+        <div className="popup">
+          <h1>Website Under Construction</h1>
+          <p>This website is currently undergoing certain changes. Please check back later.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
